632b7f1d3e4b6357dd33d644ebd7dbba9b8d07a8
Matthew VH <vanheckematt@gmail.com>
2020-05-05 13:32:24 -0500

    Added buttons for 'Previous Set' and 'Next Set' in addition to those for 'Previous Card' and 'Next Card'. Also added an h1 to display the name of the set currently in use.

--- src/stack.js
+++ src/stack.js
@@ -4,25 +4,36 @@ class Stack extends React.Component{
     constructor(props){
         super(props);
         this.state = {collection: [{"id":1,"title":"React","cards":[{"id":1,"word":"state","definition":"JS object that holds values for a component"},{"id":2,"word":"props","definition":"A way to pass data into components on initialization"},{"id":3,"word":"component","definition":"Reusable building blocks for UI using JSX"}]},{"id":2,"title":"C#","cards":[{"id":4,"word":"variable","definition":"Named space in memory"},{"id":5,"word":"class","definition":"Template for an object that consists of member variables, constructor, methods"},{"id":6,"word":"object","definition":"Instance of a class"}]},{"id":3,"title":"Flutter","cards":[{"id":7,"word":"widget","definition":"Reusable component in Flutter"}]}], stackNumber: 0, cardNumber: 0}
-        this.previous = this.previous.bind(this);
-        this.next = this.next.bind(this);
+        this.previousCard = this.previousCard.bind(this);
+        this.nextCard = this.nextCard.bind(this);
+        this.previousSet = this.previousSet.bind(this);
+        this.nextSet = this.nextSet.bind(this);
     }
     render(){
         let currentCard = this.state.collection[this.state.stackNumber].cards[this.state.cardNumber];
         console.log(currentCard);
         return(
             <div>
+                <h1>{this.state.collection[this.state.stackNumber].title}</h1>
                 <Card front={currentCard.word} back={currentCard.definition} display="" />
-                <button onClick={this.previous}>Previous</button>
-                <button onClick={this.next}>Next</button>
+                <button onClick={this.previousCard}>Previous Card</button>
+                <button onClick={this.nextCard}>Next Card</button>
+                <button onClick={this.previousSet}>Previous Set</button>
+                <button onClick={this.nextSet}>Next Set</button>
             </div>
         );
     }
-    previous(){
-        console.log("Previous");
+    previousCard(){
+        console.log("Previous Card");
     }
-    next(){
-        console.log("Next");
+    nextCard(){
+        console.log("Next Card");
+    }
+    previousSet(){
+        console.log("Previous Set");
+    }
+    nextSet(){
+        console.log("Next Set");
     }
 }
 export default Stack;
