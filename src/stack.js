fdb1eed72d2f77a1eb312b8ab107e2874da01acf
Matthew VH <vanheckematt@gmail.com>
2020-05-08 11:57:48 -0500

    Cleaned up code by removing lines of code that were no longer needed.

--- src/stack.js
+++ src/stack.js
@@ -4,26 +4,15 @@ import Axios from 'axios';
 class Stack extends React.Component{
     constructor(props){
         super(props);
-        //this.state = {collection: [{"id":1,"title":"React","cards":[{"id":1,"word":"state","definition":"JS object that holds values for a component"},{"id":2,"word":"props","definition":"A way to pass data into components on initialization"},{"id":3,"word":"component","definition":"Reusable building blocks for UI using JSX"}]},{"id":2,"title":"C#","cards":[{"id":4,"word":"variable","definition":"Named space in memory"},{"id":5,"word":"class","definition":"Template for an object that consists of member variables, constructor, methods"},{"id":6,"word":"object","definition":"Instance of a class"}]},{"id":3,"title":"Flutter","cards":[{"id":7,"word":"widget","definition":"Reusable component in Flutter"}]}], stackNumber: 0, cardNumber: 0}
         this.state = {collection: [{cards: [{definition: "", word: ""}], title: ""}], initialLoad: true, stackNumber: 0, cardNumber: 0};
         this.previousCard = this.previousCard.bind(this);
         this.nextCard = this.nextCard.bind(this);
         this.previousSet = this.previousSet.bind(this);
         this.nextSet = this.nextSet.bind(this);
         this.disableButtons = this.disableButtons.bind(this);
-        //this.showCards = this.showCards.bind(this);
     }
     render(){
-            // console.log(currentCard);
-            // if (this.state.initialLoad){
-            //     console.log('Initial Load');
-            //     return(
-            //         <button onClick={this.showCards}>Show Cards</button>
-            //     );
-            // }
-            // else{
                 let currentCard = this.state.collection[this.state.stackNumber].cards[this.state.cardNumber];
-                console.log('Later Load');
                 return(
                     <div>
                         <h1>{this.state.collection[this.state.stackNumber].title}</h1>
@@ -33,26 +22,19 @@ class Stack extends React.Component{
                         <button id="next-card" onClick={this.nextCard}>Next Card</button>
                         <button id="next-set" onClick={this.nextSet}>Next Set</button>
                     </div>
-                ); 
-            // }
-        
+                );
     }
     previousCard(){
         this.setState({cardNumber: this.state.cardNumber - 1});
-        console.log("Previous Card");
     }
     nextCard(){
         this.setState({cardNumber: this.state.cardNumber + 1});
-        console.log(this.state.collection[this.state.stackNumber].cards[this.state.cardNumber]);
-        console.log(this.state.cardNumber);
     }
     previousSet(){
         this.setState({stackNumber: this.state.stackNumber - 1, cardNumber: 0});
-        console.log("Previous Set");
     }
     nextSet(){
         this.setState({stackNumber: this.state.stackNumber + 1, cardNumber: 0});
-        console.log("Next Set");
     }
     componentDidMount(){
         this.getAllStacks();
@@ -81,12 +63,7 @@ class Stack extends React.Component{
         button.disabled = condition;
     }
     getAllStacks(){
-        // Axios.get('https://localhost:44393/api/collection').then(collectionFromAPI => console.log(collectionFromAPI.data));
         Axios.get('https://localhost:44393/api/collection').then(collectionFromAPI => this.setState({collection: collectionFromAPI.data}));
     }
-    // showCards(){
-    //     this.setState({initialLoad: false});
-    //     this.render();
-    // }
 }
 export default Stack;
