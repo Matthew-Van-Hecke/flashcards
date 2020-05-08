5c8a6ca0ed900a8931392b184c9471bdf583246e
Matthew VH <vanheckematt@gmail.com>
2020-05-08 11:53:33 -0500

    Collection of cards is now populated in an API call which is called in componentDidMount. Prior to that, the collection property of state is an empty collection which only contains the structure needed to get through the initial page load without causing errors.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import Stack from './stack';
-import logo from './logo.svg';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
         <React.StrictMode>
           <Stack />
         </React.StrictMode>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
--- src/stack.js
+++ src/stack.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import Card from './card';
+import Axios from 'axios';
 class Stack extends React.Component{
     constructor(props){
         super(props);
-        this.state = {collection: [{"id":1,"title":"React","cards":[{"id":1,"word":"state","definition":"JS object that holds values for a component"},{"id":2,"word":"props","definition":"A way to pass data into components on initialization"},{"id":3,"word":"component","definition":"Reusable building blocks for UI using JSX"}]},{"id":2,"title":"C#","cards":[{"id":4,"word":"variable","definition":"Named space in memory"},{"id":5,"word":"class","definition":"Template for an object that consists of member variables, constructor, methods"},{"id":6,"word":"object","definition":"Instance of a class"}]},{"id":3,"title":"Flutter","cards":[{"id":7,"word":"widget","definition":"Reusable component in Flutter"}]}], stackNumber: 0, cardNumber: 0}
+        //this.state = {collection: [{"id":1,"title":"React","cards":[{"id":1,"word":"state","definition":"JS object that holds values for a component"},{"id":2,"word":"props","definition":"A way to pass data into components on initialization"},{"id":3,"word":"component","definition":"Reusable building blocks for UI using JSX"}]},{"id":2,"title":"C#","cards":[{"id":4,"word":"variable","definition":"Named space in memory"},{"id":5,"word":"class","definition":"Template for an object that consists of member variables, constructor, methods"},{"id":6,"word":"object","definition":"Instance of a class"}]},{"id":3,"title":"Flutter","cards":[{"id":7,"word":"widget","definition":"Reusable component in Flutter"}]}], stackNumber: 0, cardNumber: 0}
+        this.state = {collection: [{cards: [{definition: "", word: ""}], title: ""}], initialLoad: true, stackNumber: 0, cardNumber: 0};
         this.previousCard = this.previousCard.bind(this);
         this.nextCard = this.nextCard.bind(this);
         this.previousSet = this.previousSet.bind(this);
         this.nextSet = this.nextSet.bind(this);
         this.disableButtons = this.disableButtons.bind(this);
+        //this.showCards = this.showCards.bind(this);
     }
     render(){
-        let currentCard = this.state.collection[this.state.stackNumber].cards[this.state.cardNumber];
-        // console.log(currentCard);
-        return(
-            <div>
-                <h1>{this.state.collection[this.state.stackNumber].title}</h1>
-                <Card front={currentCard.word} back={currentCard.definition} display="" />
-                <button id="previous-set" onClick={this.previousSet}>Previous Set</button>
-                <button id="previous-card" onClick={this.previousCard}>Previous Card</button>
-                <button id="next-card" onClick={this.nextCard}>Next Card</button>
-                <button id="next-set" onClick={this.nextSet}>Next Set</button>
-            </div>
-        );
+            // console.log(currentCard);
+            // if (this.state.initialLoad){
+            //     console.log('Initial Load');
+            //     return(
+            //         <button onClick={this.showCards}>Show Cards</button>
+            //     );
+            // }
+            // else{
+                let currentCard = this.state.collection[this.state.stackNumber].cards[this.state.cardNumber];
+                console.log('Later Load');
+                return(
+                    <div>
+                        <h1>{this.state.collection[this.state.stackNumber].title}</h1>
+                        <Card front={currentCard.word} back={currentCard.definition} display="" />
+                        <button id="previous-set" onClick={this.previousSet}>Previous Set</button>
+                        <button id="previous-card" onClick={this.previousCard}>Previous Card</button>
+                        <button id="next-card" onClick={this.nextCard}>Next Card</button>
+                        <button id="next-set" onClick={this.nextSet}>Next Set</button>
+                    </div>
+                ); 
+            // }
+        
     }
     previousCard(){
         this.setState({cardNumber: this.state.cardNumber - 1});
@@ -42,11 +55,15 @@ class Stack extends React.Component{
         console.log("Next Set");
     }
     componentDidMount(){
+        this.getAllStacks();
         this.disableButtons();
     }
     componentDidUpdate(){
         this.render();
-        this.disableButtons();
+        if(this.state.collection)
+        {
+            this.disableButtons();
+        }
         console.log(this.Card);
     }
     disableButtons(){
@@ -63,5 +80,13 @@ class Stack extends React.Component{
     disableIndividualButton(button, condition){
         button.disabled = condition;
     }
+    getAllStacks(){
+        // Axios.get('https://localhost:44393/api/collection').then(collectionFromAPI => console.log(collectionFromAPI.data));
+        Axios.get('https://localhost:44393/api/collection').then(collectionFromAPI => this.setState({collection: collectionFromAPI.data}));
+    }
+    // showCards(){
+    //     this.setState({initialLoad: false});
+    //     this.render();
+    // }
 }
 export default Stack;
